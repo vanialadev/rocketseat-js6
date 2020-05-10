9a43d234db25b3167b1aa3733ab6adc37758ff92
Vânia Almeida <vania.almeida28@hotmail.com>
2020-05-10 18:39:04 -0300

    Operações em Array

--- bundle.js
+++ bundle.js
@@ -1,24 +1,19 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Matematica = /*#__PURE__*/function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(x, y) {
-      return x + y;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 2));
+var arr = [1, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (item, index) {
+  return item * index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, proximo) {
+  return total + proximo;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item === 6;
+});
+console.log(find);
--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-class Matematica {
-    static soma(x, y) {
-       return x + y;
-    }
-}
+const arr = [1, 3, 4, 5, 8, 9];
 
-console.log(Matematica.soma(1,2));
+
+const newArr = arr.map(function (item, index) {
+    return item * index;
+});
+
+console.log(newArr)
+
+const sum = arr.reduce(function (total, proximo) {
+    return total + proximo;
+})
+
+console.log(sum)
+
+const filter = arr.filter(function (item) {
+    return item % 2 === 0;
+})
+
+console.log(filter);
+
+
+const find = arr.find(function (item) {
+    return item === 6;
+})
+
+console.log(find);
+
