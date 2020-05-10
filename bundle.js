68cb14b4c57362a5ef36f1f66960dc32d61bd5af
Vânia Almeida <vania.almeida28@hotmail.com>
2020-05-10 19:38:22 -0300

    Operadores rest/spread

--- bundle.js
+++ bundle.js
@@ -1,33 +1,70 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 var usuario = {
-  nome: "Diego",
+  nome: "Vania",
   idade: 23,
-  endereco: {
-    cidade: "fortaleza",
-    uf: "Ceará"
-  }
+  empresa: "Rocket"
 };
+
 var nome = usuario.nome,
-    idade = usuario.idade,
-    _usuario$endereco = usuario.endereco,
-    cidade = _usuario$endereco.cidade,
-    uf = _usuario$endereco.uf;
+    uresto = _objectWithoutProperties(usuario, ["nome"]);
+
 console.log(nome);
-console.log(idade);
-console.log(cidade);
-console.log(uf);
+console.log(usuario);
+var array = [1, 2, 3, 4];
+var a = array[0],
+    b = array[1],
+    c = array.slice(2);
+console.log(a);
+console.log(b);
+console.log(c);
 
-function mostraNome() {
-  console.log(usuario.nome);
+function soma1(a, b, c) {
+  return a + b + c;
 }
 
-function mostraNome2(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  console.log(nome);
-  console.log(idade);
+function soma2() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-mostraNome(usuario);
-mostraNome2(usuario);
+function soma3(a, b) {
+  for (var _len2 = arguments.length, params = new Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
+    params[_key2 - 2] = arguments[_key2];
+  }
+
+  return params;
+}
+
+console.log(soma2(1, 2, 3, 5));
+console.log(soma3(1, 2, 3, 5, 76, 87));
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
+var usuario1 = {
+  nome: "Vania",
+  idade: 23,
+  empresa: "Rocket"
+};
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario1), {}, {
+  nome: "Vânia Almeida"
+});
+
+console.log(usuario2);
--- main.js
+++ main.js
@@ -1,28 +1,50 @@
 const usuario = {
-  nome: "Diego",
+  nome: "Vania",
   idade: 23,
-  endereco: {
-    cidade: "fortaleza",
-    uf: "Ceará",
-  },
-};
+  empresa: "Rocket"
+}
 
-const { nome, idade, endereco: { cidade, uf } } = usuario;
+const { nome, ...uresto } = usuario;
 
 console.log(nome);
-console.log(idade);
-console.log(cidade);
-console.log(uf);
+console.log(usuario);
 
+const array = [1, 2, 3, 4];
 
-function mostraNome() {
-  console.log(usuario.nome);
+const [a, b, ...c] = array;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma1(a, b, c) {
+  return a + b + c;
 }
 
-function mostraNome2( {nome, idade}) {
-  console.log(nome);
-  console.log(idade);
+function soma2(...params) {
+  return params.reduce((total, next) => total + next);
 }
 
-mostraNome(usuario);
-mostraNome2(usuario);
+function soma3(a, b, ...params) {
+  return params;
+}
+
+console.log(soma2(1, 2, 3, 5));
+console.log(soma3(1, 2, 3, 5, 76, 87));
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+const usuario1 = {
+  nome: "Vania",
+  idade: 23,
+  empresa: "Rocket"
+}
+
+const usuario2 = { ...usuario1, nome: "Vânia Almeida" }
+
+console.log(usuario2);
