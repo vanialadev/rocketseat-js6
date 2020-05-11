35b79725c77c4ee80ea5cbdbdaa9add7ce0087b4
Vânia Almeida <vania.almeida28@hotmail.com>
2020-05-11 17:17:30 -0300

    Import/export

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-alert('teste')
+// import ClasseUsuario from './functions';
+// import { idade } from './functions';
+import ClasseUsuario, { idade as  IdadeUsuario} from './functions';
+
+// console.log(ClasseUsuario.info());
+// console.log(idade);
+
+ClasseUsuario.info();
+console.log(IdadeUsuario);
