7c5125a5deb77074e286fe013b97c68da54b8d46
Vânia Almeida <vania.almeida28@hotmail.com>
2020-05-22 17:30:21 -0300

    async/await

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
-// import ClasseUsuario from './functions';
-// import { idade } from './functions';
-import ClasseUsuario, { idade as  IdadeUsuario} from './functions';
+const minhaPromisse = () => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve(console.log('OK'))
+    resolve('OK')
+  }, 2500)
+});
 
-// console.log(ClasseUsuario.info());
-// console.log(idade);
+async function executaPromisse() {
+  const response1 = await minhaPromisse();
+  const response2 = await minhaPromisse();
+  const response3 = await minhaPromisse();
+  const response4 = await minhaPromisse();
+}
 
-ClasseUsuario.info();
-console.log(IdadeUsuario);
+async function executaPromisse2() {
+  console.log(await minhaPromisse());
+  console.log(await minhaPromisse());
+  console.log(await minhaPromisse());
+  console.log(await minhaPromisse());
+  console.log(await minhaPromisse());
+}
+
+// executaPromisse2();
+
+const executaPromisse3 = async () => {
+  console.log(await minhaPromisse());
+  console.log(await minhaPromisse());
+  console.log(await minhaPromisse());
+
+}
+executaPromisse3();
